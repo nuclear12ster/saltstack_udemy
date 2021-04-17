ed58a7e93cabadf274683d7b831a46154934a4ff
root <root@vagrant.vm>
2021-04-17 06:30:06 +0000

    saltsync

---
+++ salt_sync.sh
@@ -0,0 +1,12 @@
+
+##Clean previous configuration
+
+#sudo rm -rf /srv/*
+
+#copy salt master conf into /etc/salt/
+#sudo cp master /etc/salt
+
+#restart salt master service and check it status
+
+#sudo /etc/init.d/salt-master restart
+#sudo /etc/init.d/salt-master status
